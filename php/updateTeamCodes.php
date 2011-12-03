847021c6391499fb231fe3b18e6506b44f49aae9
Ameya Gholkar <ameya.moses@gmail.com>
2011-12-02 20:22:51 -0500

    Database version 3.0 - winner in Series now mapped to team ids.

--- php/updateTeamCodes.php
+++ php/updateTeamCodes.php
@@ -5,12 +5,12 @@ mysql_selectdb('Cricket',$link) or die('Error connecting to DB');
 //$venue_result = mysql_query("select * from venue", $link);t
 $result = mysql_query("select * from matches m where (m.team1='ENG' or m.team2='ENG') and m.type='ODI' ");
 echo "No. of matches: ".mysql_num_rows($result)."<br />";
-$result = mysql_query("select t.id as id, t.name as name from matches m, teams t where t.name = m.winner_id");
+$result = mysql_query("select t.id as id, t.code as code from series s, teams t where t.code = s.winner");
 
 while($row = mysql_fetch_assoc($result)){
 	//echo "Match : ".$row['team1']." vs ".$row['team2']." on ".$row['date']."  Type: ".$row['type']."<br />";
-	echo "Match - ".$row['id']." -> ".$row['name']."<br />";
-	mysql_query("update matches set winner_id = '".$row['id']."' where winner_id LIKE '".$row['name']."'   ");
+	echo "Match - ".$row['id']." -> ".$row['code']."<br />";
+	mysql_query("update series set winner = '".$row['id']."' where winner LIKE '".$row['code']."'   ");
 }
 echo "Total Matches played : ".mysql_num_rows($result);
 	//echo "Venue: ".$venue_row['ground_name']."<br />";
