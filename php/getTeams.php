10cc613013c98fd2bab458891070b7fc9618c3b3
Ameya Gholkar <ameya.moses@gmail.com>
2011-12-08 01:56:14 -0500

    Fix to getTeams

--- php/getTeams.php
+++ php/getTeams.php
@@ -1,7 +1,6 @@
 <?php 
 	include 'connect.php';
 	include 'send_json.php';
-	mysql_select_db('Cricket',$link) or die('Could not connect');
 	$json = array();
 	$result = mysql_query("SELECT id,name,code,latitude,longitude FROM teams WHERE code != 'ACC' AND code != 'ICC' and code != 'ACA' ORDER BY name", $link) or die('Wrong query');
 	send_json($result);
