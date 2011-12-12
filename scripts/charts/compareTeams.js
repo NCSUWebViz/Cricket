d63688f560e3cb56ec31efa892434dba7f9d5cac
mmatthews06 <mmatthews06@gmail.com>
2011-12-11 22:30:45 -0500

    start of team comparison chart migration

---
+++ scripts/charts/compareTeams.js
@@ -0,0 +1,227 @@
+var options;
+var teamsSelectSlots = new Array();
+
+
+VIS.CompareTeams = function($container) {
+    /*function loadTeams(){
+        var num = getNumberOfTeams();
+        var j = 0;
+        teamsSelectSlots = new Array();
+        //-------------------------
+        var l = 0;
+        for(l=0; l<num; l++){
+                teamsSelectSlots[l] = 0;
+        }
+        //-------------------------
+        e = document.getElementById("submit");
+        deleteElements(e);
+        e = document.getElementById("teams");
+        deleteElements(e);
+
+        id=0;
+        while(j < num) {
+            $.getJSON('php/getTeamList2.php',function populateList(data){
+                var o;
+                var to_field=document.createElement('select');
+                var label = document.createElement('h7');
+                label.innerHTML = "Team "+(id+1)+": ";
+                e.appendChild(label);
+                to_field.setAttribute('id',id.toString());
+                to_field.onchange = function(){
+                    teamValidate(this.id,this.value);
+                };
+                o = document.createElement("option");
+                o.text = "";
+                o.value = 0;
+                to_field.add(o, null);
+                var i = 0;
+                while( i < data.data.length){
+                    o = document.createElement("option");
+                    o.text = data.data[i].name;
+                    o.value = data.data[i].id;
+                    i++;
+                    to_field.add(o, null);
+                }
+                e.appendChild(to_field);
+                var br_field=document.createElement('br');
+                e.appendChild(br_field);
+                id++;
+            });
+            j++;
+        }
+        addSubmitButton();
+    }*/
+
+    function getData(){
+        var teamId = new Array();
+        var teamNames = new Array();
+        var args = "";
+        if(document.getElementById('numOfTeams') != null){
+            var num = getNumberOfTeams();
+            //alert("Selected: "+num);
+            var i = 0;
+            if( document.getElementById(i.toString()) == null ){
+                alert("null");
+            }
+            //alert("Selected: "+num);
+            while(i < num){
+                var select = document.getElementById(i.toString());
+                teamId[i] = select.options[select.selectedIndex].value;
+                teamNames[i] = select.options[select.selectedIndex].text;
+                //alert("Team Name: "+teamName);
+                i++;
+            }
+        }
+
+        i=0;
+        args += '?teams=';
+        while(i < num){
+            args += teamId[i];
+            if(i != (num-1)){
+                args += ',';
+            }
+            i++;
+        }
+
+        var type = document.getElementById('matchTypeSelect');
+        var typeText = type.options[type.selectedIndex].value;
+        if(typeText != 'All Match Types' ){
+            args += '&type=';
+            args += typeText;
+        }
+        //alert(args);
+        //--------------------Declare options-----------------
+        options =  {
+            chart: {
+                renderTo: 'container',
+                defaultSeriesType: 'spline',
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Performance Comparison of '
+            },
+            xAxis: {
+                categories: [],
+                    labels: {
+                        rotation: -45,
+                        align: 'right',
+                        step: 2,
+                        style: {
+                            font: 'normal 12px Verdana, sans-serif'
+                        }
+                    }
+            },
+            yAxis: {
+                title: {
+                    text: 'Number of Matches'
+                }
+            },
+            tooltip: {
+                    formatter: function() {
+                    var tooltip = "<b>Matches won by "+this.series.name +':</b> '+ this.y +'<br/>'+
+                    '<b>Playing Year:</b>'+ this.x + '<br /><b>' ;
+                    return tooltip;
+                    },
+                    style: {
+                    color: 'white',
+                    fontSize: '11pt',
+                    padding: '5px'
+                }
+                },
+            plotOptions: {
+                    column: {
+                    stacking: 'normal'
+                }
+            },
+            series: []
+        };
+
+        //----------------------------------------------------
+        $.getJSON('php/getCompareTeams.php'+args,function(data){
+            var series = new Array();
+            var num = getNumberOfTeams();
+            var j = 0;
+            while(j < num){
+                series[j] = {data: []};
+                series[j].name = teamNames[j];
+
+                if(data.data[0][j][teamNames[j]].length > 40){
+                    options.xAxis.labels.step = 2;
+                }else{
+                    options.xAxis.labels.step = 1;
+                }
+                if(data.data.length == 0){
+                    alert('No Data found for '+teamNames[j]);
+                }else{
+                    i = 0;
+                    while( i < data.data[0][j][teamNames[j]].length){
+                        series[j].data.push(data.data[0][j][teamNames[j]][i].wins);
+                        options.xAxis.categories.push(data.data[0][j][teamNames[j]][i].year);
+                        i++;
+                    }
+                    options.title.text += teamNames[j];
+                    if(j == (num-1)){
+                        options.title.text += ".";
+                    }else{
+                        options.title.text += " vs ";
+                    }
+                    options.series.push(series[j]);
+                }
+                j++;
+            }
+
+            options.title.text = options.title.text + " - " + typeText;
+            var chart = new Highcharts.Chart(options);
+        });
+    }
+
+    function teamValidate(id,value){
+        var num = teamsSelectSlots.length;
+        //alert(num+" "+id+" "+value);
+        var i = 0;
+        var duplicate = 0;
+        for(i = 0; i < num; i++){
+            //alert("Teamslot:"+i+" = "+teamsSelectSlots[i]);
+            if(teamsSelectSlots[i] == value){
+                if(value != 0){
+                    alert('Country already selected!');
+                }
+                var e = document.getElementById(id.toString());
+                e.selectedIndex = 0;
+                teamsSelectSlots[id] = 0;
+                duplicate = 1;
+            }
+        }
+        if(duplicate != 1){
+            //alert('Set!');
+            teamsSelectSlots[id] = value;
+        }
+    }
+
+    function getNumberOfTeams(){
+        if(document.getElementById('numOfTeams') != null){
+                var e = document.getElementById('numOfTeams');
+                    var num = e.options[e.selectedIndex].value;
+                    return num;
+            }
+    }
+
+    function addSubmitButton(){
+        var e = document.getElementById("submit");
+            var submitButton = document.createElement('input');
+            submitButton.setAttribute('value','Compare!');
+            submitButton.setAttribute('type','button');
+            submitButton.onclick = getData;
+            e.appendChild(submitButton);
+    }
+
+    function deleteElements(e){
+        if ( e.hasChildNodes() )
+        {
+            while ( e.childNodes.length >= 1 )
+            {
+                e.removeChild( e.firstChild );
+            }
+        }
+    }
+}
